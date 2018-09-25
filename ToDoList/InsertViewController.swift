59f9ffff3cc47c923e3ed22ad69e9d4f7709aab6
David Emrich <davidemrich@Davids-MacBook-Pro.local>
2018-09-25 12:08:44 -0500

    first commit:

---
+++ ToDoList/InsertViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InsertViewController.swift
+//  ToDoList
+//
+//  Created by David Emrich on 9/24/18.
+//  Copyright © 2018 David Emrich. All rights reserved.
+//
+
+import UIKit
+
+class InsertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
